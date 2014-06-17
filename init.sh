0f3dc6c893e8363abdadb5aac6a0df68343b967e
Joshua Blum <joshblum@mit.edu>
2014-06-17 15:20:40 -0700

    vim submfolder

--- init.sh
+++ init.sh
@@ -3,7 +3,8 @@ sudo pip install autopep8
 git submodule init
 git submodule update
 
-cp vimrc ~/.vimrc
+cp -R vim/ ~/.vim
+cp vim/vimrc ~/.vimrc
 cp screenrc ~/.screenrc
 cp bash_profile ~/.bash_profile
 
