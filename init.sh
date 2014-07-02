a1438a01007dd47cb61556339dfaeceecb681b3c
cantbloom <cantbloom@mit.edu>
2014-07-02 14:49:44 -0400

    backups and cleaning

--- init.sh
+++ init.sh
@@ -1,25 +1,73 @@
 #! /bin/bash
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+
+
 _done() {
 	printf "...done\n\n"
 }
 
-echo "Installing autopep8"
-sudo pip install autopep8
-_done
+# Define a timestamp function
+timestamp() {
+  date +"%T"
+}
 
-echo "Initializing submodules"
-git submodule init
-git submodule update
-_done
+backup() {
+  src=$1
+  dst=$2
+  if [ -f $dst ]; then
+    name="$dst-`timestamp`-bak"
+    echo "Saving old $dst as $name"
+    mv $dst "name"
+  fi
+  cp $src $dst
+}
 
-echo "Copying vim, screen, and bash settings"
-cp -R vim/ ~/.vim
-cp vim/vimrc ~/.vimrc
-cp screenrc ~/.screenrc
-cp bash_profile ~/.bash_profile
-source ~/.bash_profile
-_done
+install() {
+  echo "Starting installation..."
+  echo "Installing autopep8"
+  sudo pip install autopep8
+  _done
 
-source ~/.bash_profile
+  echo "Initializing submodules"
+  git submodule init
+  git submodule update
+  _done
 
+  echo "Copying vim, screen, and bash settings"
+  backup vim/ ~/.vim
+  backup vim/vimrc ~/.vimrc
+  backup screenrc ~/.screenrc
+  backup bash_profile ~/.bash_profile
+  source ~/.bash_profile
+  _done
+}
+
+clean() {
+  echo "Cleaing old files..."
+  files="~/.vim ~/.vimrc ~/.screenrc ~/.bash_profile"
+  for f in $files;
+  do
+    name="$f-*-bak"
+    echo "Removing $name"
+    rm -rf $name
+  done
+}
+
+usage() {
+  echo "Usage: ./init.sh [install|clean]"
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+if [ "$1"  == "install" ]; then
+    install
+elif [ "$1" ==  "clean" ]; then
+    clean
+else
+  usage
+fi
